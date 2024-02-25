a45a3a72bf62fab843b3ed5b94a0ca382e692611
Dominic nix <dnix8715@gmail.com>
2024-02-24 18:16:46 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+      customerName = customerName.toUpperCase();
+      return customerName;
+    } else {
+      return 'Customer name is not a string';
+    }
+  }
+  function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+  const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob';
+}
+  
